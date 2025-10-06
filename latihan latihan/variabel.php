1a3de13eb3b6f230519a305e58569e0c3006a7a6
deaajengaulia <deaaulia326@gmail.com>
2025-10-06 08:01:37 +0700

    Create variabel.php

---
+++ latihan latihan/variabel.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<HTML>
+    <HEAD>
+        <TITLE>P1</TITLE>
+    </HEAD>
+        <BODY>
+            <?php
+            $nama = "Dea Ajeng Aulia";
+            echo "$nama berkata \"Katakan namamu!\"";
+            ?>
+        </BODY>
+</HTML>
