18e50f68ee1e1b21bd65ecef55ac495cc3174d9e
deaajengaulia <deaaulia326@gmail.com>
2025-12-16 08:34:55 +0700

    Create login.php

---
+++ p9_phpmailer/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<meta charset="UTF-8">
+<title>Login | Caffe Decana</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light d-flex justify-content-center align-items-center" style="height:100vh">
+<div class="col-md-4">
+<div class="card shadow">
+<div class="card-body">
+<h4 class="text-center mb-3">☕ Caffe Decana</h4>
+<form action="proses_login.php" method="post">
+<div class="mb-3">
+<label>Email</label>
+<input type="email" name="email" class="form-control" required>
+</div>
+<div class="mb-3">
+<label>Password</label>
+<input type="password" name="password" class="form-control" required>
+</div>
+<button class="btn btn-dark w-100">Login</button>
+</form>
+<p class="text-center mt-3">
+<a href="register.php">Daftar Akun</a>
+</p>
+</div>
+</div>
+</div>
+</body>
+</html>
